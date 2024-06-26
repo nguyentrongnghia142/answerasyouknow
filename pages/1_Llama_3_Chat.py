b6fadacefc42103dd8ae0eea84650b7a06708f6d
NghiaNguyenTrong <nghiant@ihouzz.com.vn>
2024-06-26 22:08:36 +0700

    fix error: llama 3

--- pages/1_Llama_3_Chat.py
+++ pages/1_Llama_3_Chat.py
@@ -1,4 +1,6 @@
-from openai import OpenAI
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain import HuggingFaceHub
 import os
