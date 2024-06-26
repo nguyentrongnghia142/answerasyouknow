06d882dd748e402e340be283d6dcaed232e44467
NghiaNguyenTrong <nghiant@ihouzz.com.vn>
2024-06-26 22:06:45 +0700

    fix error: llama 3

--- pages/1_Llama_3_Chat.py
+++ pages/1_Llama_3_Chat.py
@@ -12,14 +12,14 @@ from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets['HUGGINGFACEHUB_API_TOKEN']
+
 model_name = "all-MiniLM-L6-v2.gguf2.f16.gguf"
 gpt4all_kwargs = {'allow_download': 'True'}
 embeddings = GPT4AllEmbeddings(
     model_name=model_name,
     gpt4all_kwargs=gpt4all_kwargs
 )
-
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets['HUGGINGFACEHUB_API_TOKEN']
 
 messages = [
     {"role": "user", "content": "Who are you?"},
